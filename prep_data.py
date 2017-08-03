e973263d439441514b93cc046bfb5cb85a5ad335
蔡健鹰 <caijy@yingmi.cn>
2017-08-03 13:03:11 +0800

    add ma_125

--- prep_data.py
+++ prep_data.py
@@ -2,6 +2,8 @@
 import quandl
 import numpy as np
 import pymysql
+import talib
+
 import xutils
 
 
@@ -20,16 +22,15 @@ if __name__ == "__main__":
 
         df['mmtm_7'] = np.log(df['Last'] / df['Last'].shift(gap))
         df['mmtm_7'] = df['mmtm_7'].fillna(0)
+        df['ma_125'] = talib.SMA(df['Last'].values, 125)
 
-        print df.tail(10)
+        # print (df.tail(10))
 
-        
-
-        upsert_sql = xutils.buildUpsertOnDuplicateSql('coin_close', ['code', 'date', 'close', 'mmtm_7'])
+        upsert_sql = xutils.buildUpsertOnDuplicateSql('coin_close', ['code', 'date', 'close', 'mmtm_7', 'ma_125'])
 
         df_tail = df.tail(10)
         for index, row in df_tail.iterrows():
-            cursor.execute(upsert_sql, (code, index.date(), float(row['Last']), float(row['mmtm_7'])) * 2)
+            cursor.execute(upsert_sql, (code, index.date(), float(row['Last']), float(row['mmtm_7']), float(row['ma_125'])) * 2)
 
     conn.commit()
     cursor.close()
