88fb916d3da03cadad94ad487c6e8eccdb634f1d
蔡健鹰 <caijy@yingmi.cn>
2017-08-05 16:52:12 +0800

    add mmtm_15

--- prep_data.py
+++ prep_data.py
@@ -18,19 +18,18 @@ if __name__ == "__main__":
     for code in codes:
         df = quandl.get("BITFINEX/" + code + "USD")
 
-        gap = 7
-
-        df['mmtm_7'] = np.log(df['Last'] / df['Last'].shift(gap))
+        df['mmtm_7'] = np.log(df['Last'] / df['Last'].shift(7))
         df['mmtm_7'] = df['mmtm_7'].fillna(0)
+        df['mmtm_15'] = np.log(df['Last'] / df['Last'].shift(15))
+        df['mmtm_15'] = df['mmtm_15'].fillna(0)
         df['ma_125'] = talib.SMA(df['Last'].values, 125)
 
-        # print (df.tail(10))
+        upsert_sql = xutils.buildUpsertOnDuplicateSql('coin_close', ['code', 'date', 'close', 'mmtm_7', 'mmtm_15', 'ma_125'])
 
-        upsert_sql = xutils.buildUpsertOnDuplicateSql('coin_close', ['code', 'date', 'close', 'mmtm_7', 'ma_125'])
+        df = df.tail(15)
 
-        df_tail = df.tail(10)
-        for index, row in df_tail.iterrows():
-            cursor.execute(upsert_sql, (code, index.date(), float(row['Last']), float(row['mmtm_7']), float(row['ma_125'])) * 2)
+        for index, row in df.iterrows():
+            cursor.execute(upsert_sql, (code, index.date(), float(row['Last']), float(row['mmtm_7']), float(row['mmtm_15']), float(row['ma_125'])) * 2)
 
     conn.commit()
     cursor.close()
