f6085c1fc24d02c7ef1477dfb786d54626d6dd5f
Unknown <sturgle@hotmail.com>
2017-12-12 19:58:07 +0800

    fix bug - not hold coin when rsi or dd too high

--- prep_data.py
+++ prep_data.py
@@ -155,7 +155,7 @@ if __name__ == "__main__":
                     if row[code + 'mmtm30'] == np.max(mmtm30_lst) and row[code] >= row[code + 'ma']:
                         pick = code
                         break
-        elif row[last_pick + 'mmtm7'] < 0:
+        elif row[last_pick + 'mmtm7'] not in mmtm7_lst:
             if len(mmtm30_lst) != 0:
                 for code in codes:
                     if row[code + 'mmtm30'] == np.max(mmtm30_lst) and row[code] >= row[code + 'ma']:
